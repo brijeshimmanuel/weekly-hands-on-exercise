47053636ef44e3e960fb4294e840c022ccc5c66b
brijeshimmanuel <145130709+brijeshimmanuel@users.noreply.github.com>
2026-06-30 14:09:06 +0530

    Add files via upload

---
+++ week-2/Exercise-1-Configuring-a-Basic-Spring-Application/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Spring will use this "setter" to inject the BookRepository
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBook() {
+        System.out.println("Book from repository: " + bookRepository.getBook());
+    }
+}
