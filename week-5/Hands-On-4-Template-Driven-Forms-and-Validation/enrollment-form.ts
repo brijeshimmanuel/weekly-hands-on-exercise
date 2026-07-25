84eb84e1ad124fbcea6756f383a9dcddafada0e5
brijeshimmanuel <145130709+brijeshimmanuel@users.noreply.github.com>
2026-07-25 17:53:22 +0530

    Add files via upload

---
+++ week-5/Hands-On-4-Template-Driven-Forms-and-Validation/enrollment-form.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-enrollment-form',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './enrollment-form.html',
+  styleUrl: './enrollment-form.css'
+})
+export class EnrollmentForm {
+
+  studentName = '';
+
+  studentEmail = '';
+
+  courseId: number | null = null;
+
+  preferredSemester = '';
+
+  agreeToTerms = false;
+
+  submitted = false;
+
+  onSubmit(form: NgForm) {
+
+    console.log(form.value);
+
+    console.log(form.valid);
+
+    this.submitted = true;
+
+  }
+
+  resetForm(form: NgForm) {
+
+    form.resetForm();
+
+    this.submitted = false;
+
+  }
+
+}
