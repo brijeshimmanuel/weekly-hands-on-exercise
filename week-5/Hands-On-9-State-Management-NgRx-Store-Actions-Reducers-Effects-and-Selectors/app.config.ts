8302e223013154c66562f3002f88859af9cbeb43
brijeshimmanuel <145130709+brijeshimmanuel@users.noreply.github.com>
2026-07-26 16:15:11 +0530

    Add files via upload

---
+++ week-5/Hands-On-9-State-Management-NgRx-Store-Actions-Reducers-Effects-and-Selectors/app.config.ts
@@ -0,0 +1,41 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
+
+import { provideRouter } from '@angular/router';
+import { provideHttpClient,withInterceptors } from '@angular/common/http';
+import { routes } from './app.routes';
+import { authInterceptor } from './interceptors/auth-interceptor';
+import { errorHandlerInterceptor } from './interceptors/error-handler-interceptor';
+import { loadingInterceptor } from './interceptors/loading-interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideState } from '@ngrx/store';
+import { courseReducer } from './store/course/course.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { CourseEffects } from './store/course/course.effects';
+import { enrollmentReducer } from './store/enrollment/enrollment.reducer';
+
+export const appConfig: ApplicationConfig = {
+  providers:[
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([
+         authInterceptor,
+         errorHandlerInterceptor,
+         loadingInterceptor
+      ])
+    ),
+    provideStore(),
+    provideState('course', courseReducer),
+    provideState('enrollment', enrollmentReducer),
+    provideEffects(CourseEffects),
+    provideStoreDevtools({
+      maxAge: 25
+    }),  
+  ]
+};
